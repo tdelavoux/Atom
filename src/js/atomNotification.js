66db668af017f9aaf6b8bb2f82e916024f26cd7c
Thibaut <thibault.delavoux@gmail.com>
2022-05-18 20:21:04 +0200

    Updating to FontAwesome 6 + and few fixes

--- src/js/atomNotification.js
+++ src/js/atomNotification.js
@@ -1,4 +1,4 @@
-(function (factory) {
+$(function (factory) {
 	if (typeof define === 'function' && define.amd) {
 		define(['jquery'], factory);
 	} else if (typeof module === 'object' && module.exports) {
