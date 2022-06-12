10a69d2ec979687943e9602accf713ae6defc285
Nathanaël Houn <contact@nathanaelhoun.fr>
2022-06-12 11:54:18 +0200

    chore: grunt minify css with sass directly

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,61 +1,50 @@
-const sass = require('dart-sass');
+const sass = require("sass");
 
 module.exports = function (grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON("package.json"),
 		uglify: {
 			options: {
-				banner: '/*! <%= pkg.name %> /<%= pkg.version %>/ <%= grunt.template.today("yyyy-mm-dd-hh:mm") %> */\n',
-				mangle: false
-			},
-			build_concat: {
-				src: "src/js/*.js",
-				dest: "dist/<%= pkg.version %>/js/<%= pkg.name %>-all.min.js",
-			},
-			build_individuals: {
-				files: [
-					{
-						expand: true,
-						cwd: "src/js/",
-						src: "*.js",
-						dest: "dist/<%= pkg.version %>/js/",
-						ext: ".min.js"
-					}
-				]
-			}
+				banner: "/*! <%= pkg.name %> /<%= pkg.version %>/ <%= grunt.template.today('yyyy-mm-dd hh:mm') %> */\n",
+			mangle: false
 		},
-		sass: {
-			options: {
-				implementation: sass,
-				sourceMap: true
-			},
-			build_unminified: {
-				files: {
-					'build/css/<%= pkg.name %>.css': 'src/css/*.scss'
-
+		build_concat: {
+			src: "src/js/*.js",
+			dest: "dist/<%= pkg.version %>/js/<%= pkg.name %>-all.min.js",
+		},
+		build_individuals: {
+			files: [
+				{
+					expand: true,
+					cwd: "src/js/",
+					src: "*.js",
+					dest: "dist/<%= pkg.version %>/js/",
+					ext: ".min.js"
 				}
-			}
+			]
+		}
+	},
+		sass: {
+		options: {
+			implementation: sass,
+			sourceMap: true,
+			outputStyle: "compressed",
 		},
-		cssmin: {
-			css: {
-				expand: true,
-				cwd: "build/css/",
-				src: "*.css",
-				dest: "dist/<%= pkg.version %>/css/",
-				ext: ".min.css"
+		build: {
+			files: {
+				"dist/<%= pkg.version %>/css/<%= pkg.name %>.css": "src/css/*.scss"
 			}
 		}
+	},
 	});
 
-	// Load the plugins
-	grunt.loadNpmTasks("grunt-contrib-uglify");
-	grunt.loadNpmTasks('grunt-sass');
-	grunt.loadNpmTasks('grunt-contrib-cssmin');
+// Load the plugins
+grunt.loadNpmTasks("grunt-contrib-uglify");
+grunt.loadNpmTasks("grunt-sass");
 
-	// Default task(s).
-	grunt.registerTask("default", [
-		"uglify",
-		"sass",
-		"cssmin",
-	]);
+// Default task(s).
+grunt.registerTask("default", [
+	"uglify",
+	"sass",
+]);
 };
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-Atom
+# Atom
+
+## Contributing
+
+The files are built using [`grunt`](https://gruntjs.com/) (run `npm install` to install it). SCSS files can be found in `src/css/`, and Javascript are in `src/js/`.
+
+To build (concatenate and minify) the files after your changes, use `npm run build`.
