2cf4bc4989d4e4a9cfb98a71d9837c980b6ae8b7
Thibaut <thibault.delavoux@gmail.com>
2022-07-04 22:01:07 +0200

    feat(js): component converted to vanilla js

--- src/js/atomComponents.js
+++ src/js/atomComponents.js
@@ -1,24 +1,22 @@
-/*###########################################################################################################
-*						Compteur de charactères pour texteArea
-############################################################################################################*/
-
-/**
+/** -----------------------------------------------------
+ *      text-area characters counter
+ * ------------------------------------------------------
  * @use optional maxlength
  *
  * Défini un compteur automatique de caractères restant pour un texte area donné.
  * La taille max est basée sur l'attribut maxlenght de la textArea. Par défaut, la valeur est à 500;
  */
-$(".a-count-area>textarea").keyup(function () {
-  $(this).parent().find(".lengthText").remove();
-  var max = $(this).attr("maxlength")
-    ? parseInt($(this).attr("maxlength"))
-    : 500;
-  var len = $(this).val().length;
-  var ch = max - len;
-  $(this)
-    .parent()
-    .prepend(
-      '<p class="small-text lengthText">' + ch + " caractère(s) restant(s)</p>"
-    );
+ document.querySelectorAll('.a-count-area>textarea').forEach(function(textarea) {
+  textarea.addEventListener('keyup', function() {
+    a_detach(this.parentNode.querySelector(".a-length-text"));
+    var max     = a_int(this.getAttribute("maxlength"), 500);
+    var len     = this.value.length;
+    var ch      = max - len;
+    var newElem = document.createElement("p");
+    newElem.classList.add("a-length-text");
+    newElem.innerHTML = `${ch} caractère(s) restant(s)`;
+    this.parentNode.prepend(newElem);
+  });
+  var event = new Event('keyup');
+  textarea.dispatchEvent(event);
 });
-$(".a-count-area>textarea").trigger("keyup");
