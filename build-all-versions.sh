8748912cdf75de65a0533f584b042ea0481aad1d
Nathanaël Houn <nathanael.houn@ca-franchecomte.fr>
2022-07-05 17:37:10 +0200

    build: adding build script for all versions

---
+++ build-all-versions.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+INITIAL_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+for TAG in $(git tag --list); do
+    echo ""
+    echo "Building version $TAG"
+    # git switch --detach "$TAG" # for next version of git
+    git checkout "$TAG" --quiet # --quiet to skip the warning about detached HEAD
+    npm run build
+done
+
+echo ""
+echo "Build finished, returning to initial branch"
+# git switch "$TAG" # for next version of git
+git checkout "$INITIAL_BRANCH"
