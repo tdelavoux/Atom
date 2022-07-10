8a3a95d0d8ed03f2e0e4a0bc0768bffc3e8c28c3
Thibaut <thibault.delavoux@gmail.com>
2022-07-10 22:25:00 +0200

    feat(js): Tabs converted to vanilla js

--- src/js/atomTabs.js
+++ src/js/atomTabs.js
@@ -1,7 +1,14 @@
-$("body").on("click", ".atom-pan-tab-header-item", function () {
-  var parent = $(this).closest(".atom-pan-tab");
-  parent.find(".atom-pan-tab-body div").removeClass("active");
-  parent.find(".atom-pan-tab-header-item").removeClass("active");
-  $(this).addClass("active");
-  $($(this).attr("data-target")).addClass("active");
+document.body.addEventListener("click", function (e) {
+  var t = e.target;
+  if (t && t.classList.contains("atom-pan-tab-header-item")) {
+    var parent = t.closest(".atom-pan-tab");
+    parent.querySelectorAll(".atom-pan-tab-body div").forEach((i) => {
+      i.classList.remove("active");
+    });
+    parent.querySelectorAll(".atom-pan-tab-header-item").forEach((i) => {
+      i.classList.remove("active");
+    });
+    t.classList.add("active");
+    document.getElementById(t.dataset.target).classList.add("active");
+  }
 });
