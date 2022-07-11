606bc2687aaf275295f0f4a83675ca93381fd321
Thibaut <thibault.delavoux@gmail.com>
2022-07-11 20:59:52 +0200

    chore(js): review vars and code optimisation

--- src/js/atomComponents.js
+++ src/js/atomComponents.js
@@ -6,17 +6,16 @@
  * Défini un compteur automatique de caractères restant pour un texte area donné.
  * La taille max est basée sur l'attribut maxlenght de la textArea. Par défaut, la valeur est à 500;
  */
- document.querySelectorAll('.a-count-area>textarea').forEach(function(textarea) {
+document.querySelectorAll('.a-count-area>textarea').forEach(function(textarea) {
   textarea.addEventListener('keyup', function() {
     a_detach(this.parentNode.querySelector(".a-length-text"));
-    var max     = a_int(this.getAttribute("maxlength"), 500);
-    var len     = this.value.length;
-    var ch      = max - len;
-    var newElem = document.createElement("p");
-    newElem.classList.add("a-length-text");
-    newElem.innerHTML = `${ch} caractère(s) restant(s)`;
-    this.parentNode.prepend(newElem);
+    const a_area_max     = a_int(this.getAttribute("maxlength"), 500);
+    const a_area_len     = this.value.length;
+    const a_area_ch      = a_area_max - a_area_len;
+    const a_area_elem    = document.createElement("p");
+    a_area_elem.classList.add("a-length-text");
+    a_area_elem.innerHTML = `${a_area_ch} caractère(s) restant(s)`;
+    this.parentNode.prepend(a_area_elem);
   });
-  var event = new Event('keyup');
-  textarea.dispatchEvent(event);
+  textarea.dispatchEvent(new Event('keyup'));
 });
