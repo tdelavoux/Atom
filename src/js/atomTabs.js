ecfdd114932fa285bfcefb66305db94f8a8fae38
Thibault Delavoux <31383617+tdelavoux@users.noreply.github.com>
2022-07-11 22:36:21 +0200

    chore(js): refactor code function

--- src/js/atomTabs.js
+++ src/js/atomTabs.js
@@ -2,10 +2,7 @@ document.body.addEventListener("click", function (e) {
   var t = e.target;
   if (t && t.classList.contains("atom-pan-tab-header-item")) {
     var parent = t.closest(".atom-pan-tab");
-    parent.querySelectorAll(".atom-pan-tab-body div").forEach((i) => {
-      i.classList.remove("active");
-    });
-    parent.querySelectorAll(".atom-pan-tab-header-item").forEach((i) => {
+    parent.querySelectorAll(".atom-pan-tab-body div, .atom-pan-tab-header-item").forEach((i) => {
       i.classList.remove("active");
     });
     t.classList.add("active");
