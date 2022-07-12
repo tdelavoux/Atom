9342e7cb4d182f1f6852dda14bf961a8f52d7920
Thibault Delavoux <31383617+tdelavoux@users.noreply.github.com>
2022-07-12 18:02:11 +0200

    feat(js): component converted to vanilla js (#12)
    * feat(js): component converted to vanilla js
    
    * chore(js): review vars and code optimisation
    
    * chore(js): refactor element
    
    Co-authored-by: Nathanaël Houn <nathanael.houn@ca-franchecomte.fr>

--- src/js/atomComponents.js
+++ src/js/atomComponents.js
@@ -1,24 +1,24 @@
-/*###########################################################################################################
-*						Compteur de charactères pour texteArea
-############################################################################################################*/
-
-/**
+/** -----------------------------------------------------
+ *      text-area characters counter
+ * ------------------------------------------------------
  * @use optional maxlength
  *
  * Défini un compteur automatique de caractères restant pour un texte area donné.
  * La taille max est basée sur l'attribut maxlenght de la textArea. Par défaut, la valeur est à 500;
  */
-$(".a-count-area>textarea").keyup(function () {
-  $(this).parent().find(".lengthText").remove();
-  var max = $(this).attr("maxlength")
-    ? parseInt($(this).attr("maxlength"))
-    : 500;
-  var len = $(this).val().length;
-  var ch = max - len;
-  $(this)
-    .parent()
-    .prepend(
-      '<p class="small-text lengthText">' + ch + " caractère(s) restant(s)</p>"
-    );
+document.querySelectorAll('.a-count-area>textarea').forEach(function (textarea) {
+    const a_area_max = a_int(this.getAttribute("maxlength"), 500);
+    const a_area_elem = document.createElement("p");
+    a_area_elem.classList.add("a-length-text");
+    a_area_elem.innerHTML = `${a_area_ch} caractères restants`;
+    this.parentNode.prepend(a_area_elem);
+
+    textarea.addEventListener('keyup', function () {
+        const a_area_len = this.value.length;
+        const remaining = a_area_max - a_area_len;
+
+        const plural = remaining > 1 ? 's' : '';
+        a_area_elem.innerText = `${remaining}/${max} caractère${plural} restant${plural}`;
+    });
+})
 });
-$(".a-count-area>textarea").trigger("keyup");
