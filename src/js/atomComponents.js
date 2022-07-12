f4607501e806bf05a791cdb612be886912895ee3
Thibault Delavoux <31383617+tdelavoux@users.noreply.github.com>
2022-07-12 18:01:48 +0200

    chore(js): refactor element
    Co-authored-by: Nathanaël Houn <nathanael.houn@ca-franchecomte.fr>

--- src/js/atomComponents.js
+++ src/js/atomComponents.js
@@ -6,16 +6,19 @@
  * Défini un compteur automatique de caractères restant pour un texte area donné.
  * La taille max est basée sur l'attribut maxlenght de la textArea. Par défaut, la valeur est à 500;
  */
-document.querySelectorAll('.a-count-area>textarea').forEach(function(textarea) {
-  textarea.addEventListener('keyup', function() {
-    a_detach(this.parentNode.querySelector(".a-length-text"));
-    const a_area_max     = a_int(this.getAttribute("maxlength"), 500);
-    const a_area_len     = this.value.length;
-    const a_area_ch      = a_area_max - a_area_len;
-    const a_area_elem    = document.createElement("p");
+document.querySelectorAll('.a-count-area>textarea').forEach(function (textarea) {
+    const a_area_max = a_int(this.getAttribute("maxlength"), 500);
+    const a_area_elem = document.createElement("p");
     a_area_elem.classList.add("a-length-text");
-    a_area_elem.innerHTML = `${a_area_ch} caractère(s) restant(s)`;
+    a_area_elem.innerHTML = `${a_area_ch} caractères restants`;
     this.parentNode.prepend(a_area_elem);
-  });
-  textarea.dispatchEvent(new Event('keyup'));
+
+    textarea.addEventListener('keyup', function () {
+        const a_area_len = this.value.length;
+        const remaining = a_area_max - a_area_len;
+
+        const plural = remaining > 1 ? 's' : '';
+        a_area_elem.innerText = `${remaining}/${max} caractère${plural} restant${plural}`;
+    });
+})
 });
