73204d7ad00c9374a13d8756acebb5359d37e1ce
Thibault Delavoux <Thibault.DELAVOUX@ca-franchecomte.fr>
2022-07-27 10:41:19 +0200

    feat(js): collapse use next element by default instead pannel

--- src/js/atomCollapse.js
+++ src/js/atomCollapse.js
@@ -2,13 +2,14 @@ $(document.body).on("click", ".a-collapsable", function () {
   const object = $(this);
   const target = object.attr("collapse-target")
     ? $(object.attr("collapse-target"))
-    : object.next(".a-panel-content"); // By default, use the a-panel-content following the header
+    : object.next(); // By default, use the a-panel-content following element
   if (target.hasClass("collapsed") || target.hasClass("a-collapsed")) {
     target.show(300);
     target.removeClass("collapsed");
     target.removeClass("a-collapsed");
   } else {
     target.addClass("collapsed");
+    target.addClass("a-collapsed");
     target.hide(300);
   }
 });
